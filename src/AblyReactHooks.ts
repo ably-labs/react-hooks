a2e503896f8a56461c3b8e5ee2a9c8ee5e5f7624
Owen Pearson <owen.pearson@ably.com>
2023-04-04 14:47:25 +0100

    fix: use object for ClientOptions.agent

--- src/AblyReactHooks.ts
+++ src/AblyReactHooks.ts
@@ -1,10 +1,14 @@
 import Ably from "ably";
 import { Types } from "ably";
 
-export type ChannelNameAndOptions = { channelName: string; options?: Types.ChannelOptions; realtime?: Types.RealtimePromise }
-export type ChannelParameters =  string | ChannelNameAndOptions;
+export type ChannelNameAndOptions = {
+  channelName: string;
+  options?: Types.ChannelOptions;
+  realtime?: Types.RealtimePromise;
+};
+export type ChannelParameters = string | ChannelNameAndOptions;
 
-const version = "2.1.0"
+const version = "2.1.0";
 
 let sdkInstance: Realtime | null = null;
 
@@ -16,23 +20,27 @@ export class Realtime extends Ably.Realtime.Promise {
       } as Types.ClientOptions;
     }
 
-    (options as any).agents = [`react-hooks/${version}`]
+    (options as any).agents = { "react-hooks": version };
 
     super(options);
   }
 }
 
 export function provideSdkInstance(ablyInstance: Types.RealtimePromise) {
-    sdkInstance = ablyInstance;
+  sdkInstance = ablyInstance;
 }
 
-export function configureAbly(ablyConfigurationObject: string | Types.ClientOptions) {
+export function configureAbly(
+  ablyConfigurationObject: string | Types.ClientOptions
+) {
   return sdkInstance || (sdkInstance = new Realtime(ablyConfigurationObject));
 }
 
 export function assertConfiguration(): Types.RealtimePromise {
   if (!sdkInstance) {
-    throw new Error('Ably not configured - please call configureAbly({ key: "your-api-key", clientId: "someid" });');
+    throw new Error(
+      'Ably not configured - please call configureAbly({ key: "your-api-key", clientId: "someid" });'
+    );
   }
 
   return sdkInstance;
